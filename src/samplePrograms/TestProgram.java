602c03ed3c8628aa73dcc35fe0bfa6d7fc6ee91f
ramprasadsuthi <ramprasadsuthi@gmail.com>
2022-06-03 08:50:00 +0530

    New Line added

--- src/samplePrograms/TestProgram.java
+++ src/samplePrograms/TestProgram.java
@@ -5,6 +5,7 @@ public class TestProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Just for Test");
+        System.out.println("This line is added from Eclipse");
 	}
 
 }
